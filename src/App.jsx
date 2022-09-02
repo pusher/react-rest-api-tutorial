efbae20fb59455b1019fa1da34e9422456bf6376
Ovie Okeh <kevinokeh@gmail.com>
2022-09-02 16:45:14 +0200

    complete axios version

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Contact from "./Contact";
 
 function App() {
+  const [contacts, setContacts] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        setError(null);
+        setContacts(response.data);
+      })
+      .catch(setError);
+
+    /* Using Fetch
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((response) => {
+        setContacts(response);
+        setError(null);
+      })
+      .catch(setError);
+    */
+  }, []);
+
+  if (error) return <p>An error occurred</p>;
+
   return (
     <div className="App">
-      <Contact />
+      {contacts.map(({ id, name, email, company }) => (
+        <Contact
+          key={id}
+          name={name}
+          email={email}
+          tagline={company.catchPhrase}
+        />
+      ))}
     </div>
   );
 }
