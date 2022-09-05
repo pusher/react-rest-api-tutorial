57735dee2df00b4052afd284c6b984a3ecb5c6c2
Ovie Okeh <kevinokeh@gmail.com>
2022-09-05 15:50:34 +0200

    add with-swr example

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import useSWR from "swr";
 import Contact from "./Contact";
 
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
 function App() {
+  const { data, error } = useSWR(
+    "https://jsonplaceholder.typicode.com/users",
+    fetcher
+  );
+
+  if (error) return <p>An error occurred</p>;
+  if (!data) return <p>Loading</p>;
+
   return (
     <div className="App">
-      <Contact />
+      {data.map(({ id, name, email, company }) => (
+        <Contact
+          key={id}
+          name={name}
+          email={email}
+          tagline={company.catchPhrase}
+        />
+      ))}
     </div>
   );
 }
