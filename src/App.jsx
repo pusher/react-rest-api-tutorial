f6bc328884245afdf118361aea5b029c5b85814c
Ovie Okeh <kevinokeh@gmail.com>
2022-09-06 12:11:26 +0200

    add react-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
 import Contact from "./Contact";
 
+const fetcher = () =>
+  fetch("https://jsonplaceholder.typicode.com/users").then((res) => res.json());
+
 function App() {
+  const { isLoading, error, data } = useQuery(["contacts"], fetcher);
+
+  if (isLoading) return <p>Loading</p>;
+  if (error) return <p>An error occurred</p>;
+
   return (
     <div className="App">
-      <Contact />
+      {data.map(({ id, name, email, company }) => (
+        <Contact
+          key={id}
+          name={name}
+          email={email}
+          tagline={company.catchPhrase}
+        />
+      ))}
     </div>
   );
 }
